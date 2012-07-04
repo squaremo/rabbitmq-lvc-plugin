03f7fdfcc7f6dd3581ae2dffd332067953be9960
Simon MacMullen <simon@rabbitmq.com>
2012-07-04 15:49:21 +0100

    Simple test for LVC.

--- package.mk
+++ package.mk
@@ -1,2 +1,3 @@
 APP_NAME=rabbitmq_lvc
 DEPS=rabbitmq-server rabbitmq-erlang-client
+WITH_BROKER_TEST_COMMANDS:=eunit:test(rabbit_lvc_test,[verbose])
---
+++ test/src/rabbit_lvc_test.erl
@@ -0,0 +1,65 @@
+%% The contents of this file are subject to the Mozilla Public License
+%% Version 1.1 (the "License"); you may not use this file except in
+%% compliance with the License. You may obtain a copy of the License
+%% at http://www.mozilla.org/MPL/
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and
+%% limitations under the License.
+%%
+%% The Original Code is RabbitMQ.
+%%
+%% The Initial Developer of the Original Code is VMware, Inc.
+%% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
+%%
+
+-module(rabbit_lvc_test).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+lvc_test() ->
+    {ok, Conn} = amqp_connection:start(#amqp_params_network{}),
+    {ok, Ch} = amqp_connection:open_channel(
+                 Conn, {amqp_direct_consumer, [self()]}),
+    X = <<"test-lvc-exchange">>,
+    RK = <<"test">>,
+    Payload = <<"Hello world">>,
+    exchange_declare(Ch, X),
+    Q1 = queue_declare(Ch),
+    Q2 = queue_declare(Ch),
+    bind(Ch, X, RK, Q1),
+    publish(Ch, X, RK, Payload),
+    bind(Ch, X, RK, Q2),
+    expect(Ch, Q1, Payload),
+    expect(Ch, Q2, Payload),
+    amqp_connection:close(Conn).
+
+exchange_declare(Ch, X) ->
+    amqp_channel:call(Ch, #'exchange.declare'{exchange    = X,
+                                              type        = <<"x-lvc">>,
+                                              auto_delete = true}).
+
+queue_declare(Ch) ->
+    #'queue.declare_ok'{queue = Q} =
+        amqp_channel:call(Ch, #'queue.declare'{exclusive = true}),
+    Q.
+
+publish(Ch, X, RK, Payload) ->
+    amqp_channel:cast(Ch, #'basic.publish'{exchange    = X,
+                                           routing_key = RK},
+                      #amqp_msg{payload = Payload}).
+
+expect(Ch, Q, Payload) ->
+    #'basic.consume_ok'{consumer_tag = CTag} =
+        amqp_channel:call(Ch, #'basic.consume'{queue = Q}),
+    receive
+        {#'basic.deliver'{consumer_tag = CTag}, #amqp_msg{payload = Payload}} ->
+            ok
+    end.
+
+bind(Ch, X, RK, Q) ->
+    amqp_channel:call(Ch, #'queue.bind'{queue       = Q,
+                                        exchange    = X,
+                                        routing_key = RK}).
