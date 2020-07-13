ce29b266b9fa9bd0b044fbc78df194f27ec871a8
dcorbacho <dparracorbacho@piotal.io>
2020-07-13 15:31:44 +0100

    Switch to Mozilla Public License 2.0 (MPL 2.0)

--- src/rabbit_exchange_type_lvc.erl
+++ src/rabbit_exchange_type_lvc.erl
@@ -1,3 +1,8 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%
+%% Copyright (c) 2007-2012 VMware, Inc.  All rights reserved.
 -module(rabbit_exchange_type_lvc).
 -include_lib("rabbit_common/include/rabbit.hrl").
 -include_lib("rabbit/include/amqqueue.hrl").
